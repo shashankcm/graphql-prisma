f4f4fcd039307de2cdc9aa88034784cd1c24fb68
shashank <shashank@esystems-inc.com>
2019-07-01 14:50:11 -0400

    Create Filtering for Links

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,5 +1,16 @@
-const feed = (root, args, context, info) => {
-  return context.prisma.links();
+const feed = async (root, args, context, info) => {
+  const where = args.filter
+    ? {
+        OR: [
+          { description_contains: args.filter },
+          { url_contains: args.filter }
+        ]
+      }
+    : {};
+  const links = context.prisma.links({
+    where
+  });
+  return links;
 };
 const info = () => `This is an Hakernews API clone`;
 
