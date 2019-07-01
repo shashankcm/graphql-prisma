b23f8018e61e2228c081d1ed09a812cc94ab5327
shashank <shashank@esystems-inc.com>
2019-07-01 16:42:35 -0400

    Create Sort Functionality to sort Links.

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -10,7 +10,8 @@ const feed = async (root, args, context, info) => {
   const links = context.prisma.links({
     where,
     skip: args.skip,
-    first: args.first
+    first: args.first,
+    orderBy: args.orderBy
   });
   return links;
 };
