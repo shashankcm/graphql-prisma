7a629dce97d1c668da3e72279d40d4e1fa8be24d
shashank <shashank@esystems-inc.com>
2019-07-10 13:34:55 -0400

    Implemented New vote and new link subscription

--- client/src/components/LinkList.js
+++ client/src/components/LinkList.js
@@ -25,6 +25,53 @@ export const FEED_QUERY = gql`
     }
   }
 `
+const NEW_LINKS_SUBSCRIPTION = gql`
+  subscription {
+    newLink {
+      id
+      url
+      description
+      createdAt
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
+    }
+  }
+`
+
+const NEW_VOTES_SUBSCRIPTION = gql`
+  subscription {
+    newVote {
+      id
+      link {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`
 
 class LinkList extends React.Component {
   _updateCacheAfterVote = (store, createVote, linkId) => {
@@ -36,12 +83,38 @@ class LinkList extends React.Component {
 
     store.writeQuery({ query: FEED_QUERY, data })
   }
+  _subscribeToNewLinks = async subscribeToMore => {
+    subscribeToMore({
+      document: NEW_LINKS_SUBSCRIPTION,
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData.data) return prev
+        const newLink = subscriptionData.data.newLink
+        const exists = prev.feed.links.find(({ id }) => id === newLink.id)
+        if (exists) return prev
+
+        return Object.assign({}, prev, {
+          feed: {
+            links: [newLink, ...prev.feed.links],
+            count: prev.feed.links.length + 1,
+            __typename: prev.feed.__typename,
+          },
+        })
+      },
+    })
+  }
+  _subscribeToNewVotes = async subscribeToMore => {
+    subscribeToMore({
+      document: NEW_VOTES_SUBSCRIPTION,
+    })
+  }
   render() {
     return (
       <Query query={FEED_QUERY}>
-        {({ loading, error, data }) => {
+        {({ loading, error, data, subscribeToMore }) => {
           if (error) return <div>Error</div>
           if (loading) return <div>Fetching Data</div>
+          this._subscribeToNewLinks(subscribeToMore)
+          this._subscribeToNewVotes(subscribeToMore)
           const linksToBeRendered = data.feed.links
           return (
             <div>
